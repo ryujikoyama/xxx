64a60739f13a03a5c3c0d538216d914db51b745b
ryujikoyama <64805779+ryujikoyama@users.noreply.github.com>
2021-02-25 21:16:54 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+  <!-- hooter -->
+  <hooter>
+    <div class="hooter-wrapper">
+    <div class="Copyright">
+      <p>Copyright &copy; 2021 Ryuji Koyama All Rights Reserved.</p>
+    </div>
+  </div>
+  </hooter>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" rel="stylesheet" type="text/css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" rel="stylesheet" type="text/css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.1.0/jquery-migrate.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" type="text/javascript"></script>
+  <?php wp_footer(); ?>
+  </body>
+</html>
